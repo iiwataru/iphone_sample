4b4af804ad89ab496a231815ee62cd9d6f0e50a0
ohta_tsutomu <ohta@tangit.jp>
2012-07-11 19:19:19 +0900

    カメラでの写真撮影

---
+++ Camera/Camera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by Tsutomu Ohta on 12/07/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
