02f0d9e794ce88cca322a48dee5f3f84d5a67141
iida wataru <iida.wataru@gmail.com>
2012-07-11 15:54:28 +0900

    PickerView追加

---
+++ PickerView/PickerView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PickerView
+//
+//  Created by 飯田 亘 on 12/07/11.
+//  Copyright (c) 2012年 Tangit Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
