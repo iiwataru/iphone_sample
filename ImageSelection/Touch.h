560cd1d5be9ea9732e4217a1365bd7a7b7461563
ohta_tsutomu <ohta@tangit.jp>
2012-07-12 12:06:34 +0900

    画像選択時の枠表示

---
+++ ImageSelection/Touch.h
@@ -0,0 +1,13 @@
+//
+//  Touch.h
+//  ImageSelection
+//
+//  Created by Tsutomu Ohta on 12/07/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Touch : UIImageView
+
+@end
---
+++ ImageSelection/Touch.m
@@ -0,0 +1,31 @@
+//
+//  Touch.m
+//  ImageSelection
+//
+//  Created by Tsutomu Ohta on 12/07/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Touch.h"
+
+@implementation Touch
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
