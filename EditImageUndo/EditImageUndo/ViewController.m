2de1f9eb701a93feb674c038a05903387f0e93be
ohta_tsutomu <ohta@tangit.jp>
2012-07-17 10:01:23 +0900

    画像編集のUndo処理

---
+++ EditImageUndo/EditImageUndo/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  EditImageUndo
+//
+//  Created by Tsutomu Ohta on 12/07/13.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize appleImage;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setAppleImage:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)undoImage:(id)sender {
+    [appleImage UndoSubView];
+}
+@end
