e33c1618d61ffe4eef9269aa12c830c0f9b3512d
ohta_tsutomu <ohta@tangit.jp>
2012-07-24 15:14:30 +0900

    誕生日スライドのアニメーションを追加

---
+++ SlideBirthDay/SlideBirthDay/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SlideBirthDay
+//
+//  Created by Tsutomu Ohta on 12/07/23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+- (IBAction)btnreplay:(id)sender;
+@end
---
+++ SlideBirthDay/SlideBirthDay/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  SlideBirthDay
+//
+//  Created by Tsutomu Ohta on 12/07/23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize webview;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"www/pages/test" ofType:@"html"];
+    [webview loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]]];
+}
+
+- (void)viewDidUnload
+{
+    [self setWebview:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)btnreplay:(id)sender {
+    [webview stringByEvaluatingJavaScriptFromString:@"setSlideshow()"];
+}
+@end
